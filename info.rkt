2875e14c410016db715e8e6418027968e2e1b617
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2014-11-28 18:06:10 -0500

    Disable files that break on DrDr.

--- info.rkt
+++ info.rkt
@@ -4,6 +4,11 @@
 (define name "DrDr")
 (define compile-omit-paths 'all)
 
+(define test-omit-paths '("archive-repair.rkt" "cc.rkt" "diffcmd.rkt"
+                          "housecall.rkt" "main.rkt" "make-archive.rkt"
+                          "monitor-drdr.rkt" "render.rkt" "replay-log.rkt"
+                          "time-file.rkt" "time.rkt" "vdbm.rkt"))
+
 (define test-responsibles '((all jay)))
 (define deps '("base"
                "eli-tester"
