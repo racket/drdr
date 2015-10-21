76751f8d9efa91b45690af9ac6ab10c52abd6712
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-10-21 17:00:52 -0400

    Save every 100th build as a tarball.

--- make-archive-lib.rkt
+++ make-archive-lib.rkt
@@ -1,6 +1,5 @@
 #lang racket/base
 (require racket/system
-         racket/local
          racket/file
          "config.rkt"
          "archive.rkt"
@@ -9,17 +8,21 @@
 
 (define (make-archive rev)
   (define archive-path (revision-archive rev))
-  (if (file-exists? archive-path)
-    (begin (printf "r~a is already archived\n" rev)
-           #t)
-    (begin (local [(define tmp-path (make-temporary-file))]
-                  (printf "Archiving r~a\n" rev)
-                  (safely-delete-directory (revision-trunk.tgz rev))
-                  (safely-delete-directory (revision-trunk.tar.7z rev))
-                  (create-archive tmp-path (revision-dir rev))
-                  (rename-file-or-directory tmp-path archive-path)
-                  (safely-delete-directory (revision-log-dir rev))
-                  (safely-delete-directory (revision-analyze-dir rev)))
-           #f)))
+  (cond [(file-exists? archive-path)
+         (printf "r~a is already archived\n" rev)
+         #t]
+        [(zero? (modulo rev 100))
+         (printf "r~a is saved for posterity\n" rev)
+         #t]
+        [else
+         (define tmp-path (make-temporary-file))
+         (printf "Archiving r~a\n" rev)
+         (safely-delete-directory (revision-trunk.tgz rev))
+         (safely-delete-directory (revision-trunk.tar.7z rev))
+         (create-archive tmp-path (revision-dir rev))
+         (rename-file-or-directory tmp-path archive-path)
+         (safely-delete-directory (revision-log-dir rev))
+         (safely-delete-directory (revision-analyze-dir rev))
+         #f]))
 
 (provide make-archive)
